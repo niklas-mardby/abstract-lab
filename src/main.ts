5bb0a8e861b8f7c97b8af0dde4271ba06493323d
Niklas Mårdby <niklas.mardby@gmail.com>
2025-01-10 10:30:52 +0100

    Added some demo code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import "./style.css";
+import { logSum } from "./utils";
+
+logSum(2, 2);
+logSum(5, 2); // vi byter från console.log(5 + 2); till funktionsanropet
+console.log(12 + 2);
+console.log(20 + 20);
+console.log(50 + 20);
---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export const logSum = (a: number, b: number) => {
+	console.log(a + b);
+};
+
+export const logDiff = (a: number, b: number) => {
+	console.log(a - b);
+};
+
+// utils = utility, tänk verktyg
